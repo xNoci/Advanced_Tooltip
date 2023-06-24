9bd91e4cfcba107f3ace6215af3363d7db959a3b
Noci <nocigit@gmail.com>
2023-06-24 11:35:00 +0200

    Made better use of optional

--- core/src/main/java/me/noci/advancedtooltip/core/listener/ItemStackTooltipListener.java
+++ core/src/main/java/me/noci/advancedtooltip/core/listener/ItemStackTooltipListener.java
@@ -3,7 +3,6 @@ package me.noci.advancedtooltip.core.listener;
 import me.noci.advancedtooltip.core.AdvancedTooltipAddon;
 import me.noci.advancedtooltip.core.config.AdvancedTooltipConfiguration;
 import me.noci.advancedtooltip.core.referenceable.ItemQuery;
-import me.noci.advancedtooltip.core.utils.MapLocation;
 import net.labymod.api.Laby;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.component.format.TextColor;
@@ -11,13 +10,11 @@ import net.labymod.api.client.world.effect.PotionEffect;
 import net.labymod.api.client.world.item.ItemStack;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.world.ItemStackTooltipEvent;
-import net.labymod.api.nbt.NBTTagType;
 import net.labymod.api.nbt.tags.NBTTagCompound;
 import net.labymod.api.util.I18n;
 import net.labymod.api.util.time.TimeUtil;
 
 import java.util.List;
-import java.util.Optional;
 
 public class ItemStackTooltipListener {
 
@@ -62,22 +59,22 @@ public class ItemStackTooltipListener {
     }
 
     private void handleShowNbtData(ItemStack itemStack, List<Component> tooltip) {
-        Optional<String> nbt = itemQuery.getItemNBTData(itemStack, config.developerSettings().printWithArrayData().get().isPressed());
-        if (nbt.isEmpty()) {
-            tooltip(tooltip, "no_nbt_data");
-            return;
-        }
-
-        tooltip(tooltip, false, "");
-        for (String s : nbt.get().split("\n")) {
-            tooltip(tooltip, false, s);
-        }
+        boolean withArrayContent = config.developerSettings().printWithArrayData().get().isPressed();
+        itemQuery.getItemNBTData(itemStack, withArrayContent)
+                .ifPresentOrElse(nbt -> {
+                    tooltip(tooltip, false, "");
+                    for (String s : nbt.split("\n")) {
+                        tooltip(tooltip, false, s);
+                    }
+                }, () -> {
+                    tooltip(tooltip, "no_nbt_data");
+                });
     }
 
     private void handleShowDurability(ItemStack itemStack, List<Component> tooltip) {
         if (itemStack.getMaximumDamage() <= 0) return;
         NBTTagCompound tag = itemStack.getNBTTag();
-        if(tag != null && tag.contains("Unbreakable")) return;
+        if (tag != null && tag.contains("Unbreakable")) return;
         tooltip(tooltip, "durability", itemStack.getMaximumDamage() - itemStack.getCurrentDamageValue(), itemStack.getMaximumDamage());
     }
 
@@ -88,30 +85,28 @@ public class ItemStackTooltipListener {
     }
 
     private void handleDiscSignalStrength(ItemStack itemStack, List<Component> tooltip) {
-        Optional<Integer> strength = itemQuery.getDiscSignalStrengt(itemStack);
-        if (strength.isEmpty()) return;
-        tooltip(tooltip, "disc_signal_strength", strength.get());
+        itemQuery.getDiscSignalStrengt(itemStack)
+                .ifPresent(strength -> tooltip(tooltip, "disc_signal_strength", strength));
     }
 
     private void handleExplorerMap(ItemStack itemStack, List<Component> tooltip) {
-        Optional<MapLocation> mapLocation = itemQuery.getExplorerMapLocation(itemStack);
-        if (mapLocation.isEmpty()) return;
-        tooltip(tooltip, "explorer_map." + mapLocation.get().getType().name().toLowerCase(), mapLocation.get().getX(), mapLocation.get().getZ());
+        itemQuery.getExplorerMapLocation(itemStack)
+                .ifPresent(mapLocation -> tooltip(tooltip, "explorer_map." + mapLocation.getType().name().toLowerCase(), mapLocation.getX(), mapLocation.getZ()));
     }
 
     private void handleSuspiciousStewEffect(ItemStack itemStack, List<Component> tooltip) {
-        Optional<List<PotionEffect>> stewEffects = itemQuery.getStewEffect(itemStack);
-        if (stewEffects.isEmpty()) return;
+        itemQuery.getStewEffect(itemStack)
+                .ifPresent(stewEffects -> {
+                    for (PotionEffect stewEffect : stewEffects) {
+                        String name = Laby.labyAPI().minecraft().getTranslation(stewEffect.getTranslationKey());
+                        String duration = TimeUtil.formatTickDuration(stewEffect.getDuration());
+                        if (stewEffect.isInfiniteDuration()) {
+                            duration = I18n.translate("advancedtooltip.tooltip.potion_effect.duration_infinity");
+                        }
 
-        for (PotionEffect stewEffect : stewEffects.get()) {
-            String name = Laby.labyAPI().minecraft().getTranslation(stewEffect.getTranslationKey());
-            String duration = TimeUtil.formatTickDuration(stewEffect.getDuration());
-            if (stewEffect.isInfiniteDuration()) {
-                duration = I18n.translate("advancedtooltip.tooltip.potion_effect.duration_infinity");
-            }
-
-            tooltip(tooltip, false, I18n.translate("advancedtooltip.tooltip.potion_effect.display", name, duration));
-        }
+                        tooltip(tooltip, false, I18n.translate("advancedtooltip.tooltip.potion_effect.display", name, duration));
+                    }
+                });
     }
 
     private void tooltip(List<Component> tooltip, String key, Object... value) {
