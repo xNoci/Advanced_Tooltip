c1f880eae3bea4dd11648a503678fe8ee44364e2
Noci <nocigit@gmail.com>
2024-03-22 17:48:32 +0100

    fix: added support for Tool Component

--- game-runner/src/v24w12a/java/me/noci/advancedtooltip/v24w12a/components/ComponentUtils.java
+++ game-runner/src/v24w12a/java/me/noci/advancedtooltip/v24w12a/components/ComponentUtils.java
@@ -437,6 +437,22 @@ public class ComponentUtils {
             return ComponentPrinter.component(component, ComponentPrinter.object("rarity", components).inline());
         }
 
+        if (component.value() instanceof Tool tool) {
+            var rulesComponents = tool.rules().stream().map(rule -> {
+                List<ComponentPrinter> ruleComponents = Lists.newArrayList();
+                ruleComponents.add(ComponentPrinter.expandableList("blocks", rule.blocks().stream().map(Holder::getRegisteredName).toList()));
+                rule.speed().ifPresent(speed -> ruleComponents.add(ComponentPrinter.value("speed", speed)));
+                rule.correctForDrops().ifPresent(correctForDrops -> ruleComponents.add(ComponentPrinter.value("correct_for_drops", correctForDrops)));
+                return ComponentPrinter.object(ruleComponents);
+            }).toList();
+
+            return ComponentPrinter.component(component, ComponentPrinter.object(
+                    ComponentPrinter.value("mining_speed", tool.defaultMiningSpeed()),
+                    ComponentPrinter.value("damage_per_block", tool.damagePerBlock()),
+                    ComponentPrinter.list("rules", rulesComponents).handler(ComponentPrinter::print)
+            ).inline(tool.rules().isEmpty()));
+        }
+
         return ComponentPrinter.component(component, ComponentPrinter.unsupported());
     }
 
