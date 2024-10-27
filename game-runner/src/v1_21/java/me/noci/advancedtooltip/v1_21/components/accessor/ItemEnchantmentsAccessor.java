1ad3a6bbd1764400c9350ed7812c8c7dbff7465a
Noci <nocigit@gmail.com>
2024-10-27 18:09:58 +0100

    refactor: improved getting item enchantment for mining speed

--- game-runner/src/v1_21/java/me/noci/advancedtooltip/v1_21/components/accessor/ItemEnchantmentsAccessor.java
+++ game-runner/src/v1_21/java/me/noci/advancedtooltip/v1_21/components/accessor/ItemEnchantmentsAccessor.java
@@ -1,26 +1,13 @@
 package me.noci.advancedtooltip.v1_21.components.accessor;
 
 import net.minecraft.core.Holder;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 
-import java.util.List;
 import java.util.Set;
 
 public interface ItemEnchantmentsAccessor {
     boolean isShownInTooltip();
 
     Set<Holder<Enchantment>> enchantments();
-
-    default int getLevel(ItemStack itemStack, ResourceKey<Enchantment> enchantmentResourceKey) {
-
-        for (Holder<Enchantment> enchantment : enchantments()) {
-            if (!enchantment.is(enchantmentResourceKey)) continue;
-            return itemStack.getEnchantments().getLevel(enchantment);
-        }
-
-        return 0;
-    }
 
 }
